0beafbe0d0e6a4f89a2fa0a992de40e96f9715b0
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-05-08 13:03:22 +0700

    fix: :bug: fix card bug

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,20 +1,24 @@
 import Review from "./Review";
 import Work from "./Work";
 import Portfolio from "./Portfolio";
+import React from "react";
 
 interface CardProps {
   contentType: string;
   content: any[];
 }
 
-const Card = ({ contentType, content }: CardProps) =>
-  content.map(
-    (item: any) =>
-      ({
-        Testimonials: <Review item={item} key={item} />,
-        "Recent Work": <Work item={item} key={item} />,
-        Portfolio: <Portfolio item={item} key={item} />,
-      }[contentType])
-  );
+const Card = ({ contentType, content }: CardProps) => (
+  <React.Fragment>
+    {content.map(
+      (item: any) =>
+        ({
+          Testimonials: <Review item={item} key={item} />,
+          "Recent Work": <Work item={item} key={item} />,
+          Portfolio: <Portfolio item={item} key={item} />,
+        }[contentType])
+    )}
+  </React.Fragment>
+);
 
 export default Card;
